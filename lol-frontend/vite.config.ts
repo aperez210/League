63809d2b2e8d0e1ea40149ff5d8bc0768993ab7a
Mark S <maschlining@yahoo.com>
2026-05-23 12:22:06 -0400

    update packages and tailwind

--- lol-frontend/vite.config.ts
+++ lol-frontend/vite.config.ts
@@ -1,16 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		proxy: {
 			// Whenever Svelte makes a request to /api/...
 			'/api': {
 				// ...Vite forwards it to your FastAPI server
-				target: 'http://127.0.0.1:8000', 
+				target: 'http://127.0.0.1:8000',
 				changeOrigin: true,
-				
+
 				// OPTIONAL: If your FastAPI routes do NOT start with /api 
 				// (e.g., your python route is just @app.get("/champions")),
 				// uncomment the line below to strip "/api" before sending it to Python:
